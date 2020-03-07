ce25e52c22776deeab770acf4ce7e24ac4f531d4
gabrielayala97 <54210368+gabrielayala97@users.noreply.github.com>
2020-03-07 13:13:19 -0300

    Dashboard
    Implementacion del Dashboard por medio de plantillas de bootstrap

--- gestionProyectos/gestionProyectos/urls.py
+++ gestionProyectos/gestionProyectos/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from apps.usuario import views
+
 
 urlpatterns = [
 	path('', views.welcome),
 	path('register', views.register),
-	path('login',views.login),
+	path('login', views.login),
 	path('logout/',views.logout),
 	
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
