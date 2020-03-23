8c066f4f7671ace513449e2df1e017cf20773adb
juanMartinez98k <61430571+juanMartinez98k@users.noreply.github.com>
2020-03-22 23:12:20 -0400

    Update views.py

--- gestionProyectos/apps/proyecto/views.py
+++ gestionProyectos/apps/proyecto/views.py
@@ -19,6 +19,25 @@ def listar_proyecto(request):
 	proyecto = Proyecto.objects.all().order_by('id')
 	return render(request, 'proyecto/listar_proyecto.html',{'Proyectos': proyecto})
 
+def editar_proyecto(request, id_proyecto):
+    # Recuperamos la instancia del proyecto
+    instancia = Proyecto.objects.get(id=id_proyecto)
+
+    if request.method == "GET":
+        # Actualizamos el formulario con los datos recibidos
+        form = ProyectoForm(instance=instancia)
+    else:
+        form = ProyectoForm(request.POST,instance=instancia)
+        if form.is_valid():
+            form.save()
+        return redirect('/listar_proyecto')
+    return render(request, 'proyecto/proyecto_form.html',{'form': form})
+
+def eliminar_proyecto(request,id_proyecto):
+    proyecto = Proyecto.objects.get(id = id_proyecto)
+    proyecto.delete()
+    return redirect('/listar_proyecto')
+
 '''def asignar_proyecto(request):
     if request.method == 'POST':
         form = ProyectoUsuarioForm(request.POST)
