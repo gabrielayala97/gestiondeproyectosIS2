36ace276ed0ce91a099540e9764752f43801128d
juanMartinez98k <61430571+juanMartinez98k@users.noreply.github.com>
2020-03-22 23:12:59 -0400

    Update urls.py

--- gestionProyectos/apps/proyecto/urls.py
+++ gestionProyectos/apps/proyecto/urls.py
@@ -4,6 +4,6 @@ from apps.proyecto import views
 urlpatterns = [
 	path('registrar_proyecto/', views.registrar_proyecto),
 	path('listar_proyecto/', views.listar_proyecto),
-    #path('editar_proyecto/<id_proyecto>/', views.editar_proyecto, name='editar_proyecto'),
-    #path('eliminar_proyecto/<id_proyecto>/', views.eliminar_proyecto, name='eliminar_proyecto'),
+    	path('editar_proyecto/<id_proyecto>/', views.editar_proyecto, name='editar_proyecto'),
+    	path('eliminar_proyecto/<id_proyecto>/', views.eliminar_proyecto, name='eliminar_proyecto'),
 ]
