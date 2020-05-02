f4c95e36b6d71fec11a42fa1d4f58cef667bc2a3
gabrielayala97 <54210368+gabrielayala97@users.noreply.github.com>
2020-05-02 19:34:28 -0400

    Actualizacion de Linea Base
    Creacion de una Linea Base, ademas de agregar tareas a la linea base.

--- gestionProyectos/apps/linea_base/views.py
+++ gestionProyectos/apps/linea_base/views.py
@@ -6,4 +6,22 @@ from apps.proyecto.models import Proyecto
 from apps.tarea.models import Tarea
 from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login')
+def crear_linea_base(request):
+	if request.method == 'POST':
+		form = LineaBaseForm(request.POST)
+		if form.is_valid():
+			form.save()
+		return redirect('/')
+	else:
+		form = LineaBaseForm()
+	return render(request, 'linea_base/linea_base_form.html',{'form': form})
 
+@login_required(login_url='/login')
+def agregar_tareas(request):
+    if request.user.is_superuser :
+        proyecto = Proyecto.objects.all().order_by('id')
+        return render(request, 'linea_base/seleccion_proyecto.html',{'Proyectos': proyecto})
+    else:
+        proyecto = Proyecto.objects.filter(usuarios = request.user.id)
+        return render(request, 'linea_base/seleccion_proyecto.html',{'Proyectos': proyecto})
